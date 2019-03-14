1ecf1c87ad24aa4980c4ef0f6da2bd453d3f7234
review <1326368622@qq.com>
2019-03-14 09:05:44 +0800

    输出不同的日期丶时间格式的练习代码

---
+++ src/com/soft1841/punch3/DateFormatTest.java
@@ -0,0 +1,36 @@
+package com.soft1841.punch3;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * 输出不同的日期丶时间格式的练习
+ *
+ * @author 屏儿
+ * 2019/03/14
+ */
+public class DateFormatTest {
+    public static void main(String[] args) {
+        //创建日期
+        Date date = new Date();
+        //创建不同的日期格式
+        DateFormat df1 = DateFormat.getInstance();
+        DateFormat df2 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss EE");
+        DateFormat df3 = new SimpleDateFormat("yyyy年MM月dd日 hh时mm分ss秒 EE", Locale.CHINA);
+        DateFormat df4 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss EE", Locale.US);
+        DateFormat df5 = new SimpleDateFormat("yyyy-MM-dd");
+        DateFormat df6 = new SimpleDateFormat("yyyy年MM月dd日");
+        //将日期按照不同的格式进行输出
+        System.out.println("-------将日期按照不同的格式进行输出-------");
+        System.out.println("按照Java默认的日期格式:" + df1.format(date));
+        System.out.println("按照Java默认的日期格式:" + df1.format(date));
+        System.out.println("按照指定格式 yyyy-MM-dd hh:mm:ss EE,系统默认区域：" + df2.format(date));
+        System.out.println("按照指定格式 yyyy年MM月dd日 hh时mm分ss秒 EE,区域中国：" + df3.format(date));
+        System.out.println("按照指定格式 yyyy-MM-dd hh:mm:ss EE,区域为美国：" + df4.format(date));
+        System.out.println("按照指定格式 yyyy-MM-dd:" + df5.format(date));
+        System.out.println("按照指定格式 yyyy年MM月dd日：" + df6.format(date));
+    }
+}
